2282a59a90e45c47542c251ae59764f6d01266ee
seo0 <qwee0328@naver.com>
2023-01-30 17:45:45 +0900

    회원가입 타입 버튼액션 추가

--- app/src/main/java/com/gupsa/allocate/LoginActivity.java
+++ app/src/main/java/com/gupsa/allocate/LoginActivity.java
@@ -2,6 +2,7 @@ package com.gupsa.allocate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,6 +26,14 @@ public class LoginActivity extends AppCompatActivity {
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             intent.putExtra(Constants.INTENT_EXTRA_USER_TYPE, UserType.EMPLOYEE);
             startActivity(intent);
+        });
+
+        binding.btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
---
+++ app/src/main/java/com/gupsa/allocate/SignupActivity.java
@@ -0,0 +1,32 @@
+package com.gupsa.allocate;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.AdaptiveIconDrawable;
+import android.os.Bundle;
+
+import com.gupsa.allocate.databinding.ActivityLoginBinding;
+import com.gupsa.allocate.databinding.ActivitySignupBinding;
+import com.gupsa.allocate.utils.Constants;
+import com.gupsa.allocate.utils.UserType;
+
+public class SignupActivity extends AppCompatActivity {
+
+    ActivitySignupBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnArrowSignup.setOnClickListener(v -> {
+            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+            intent.putExtra(Constants.INTENT_EXTRA_USER_TYPE, UserType.EMPLOYEE);
+            startActivity(intent);
+        });
+    }
+}
