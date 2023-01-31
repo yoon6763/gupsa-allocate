a17553b10ed6a61d4f08ac8f2ea34eb66c0a6f89
uknow-laptop <yoon6763@naver.com>
2023-01-31 11:45:38 +0900

    배차 조회 타입 Enum 클래스 추가

---
+++ app/src/main/java/com/gupsa/allocate/utils/AllocationViewType.java
@@ -0,0 +1,28 @@
+package com.gupsa.allocate.utils;
+
+public enum AllocationViewType {
+
+    /*
+        EMPLOYEE
+     */
+
+    EMP_HOME_TODAY,
+    EMP_HOME_ALL,
+    EMP_HOME_NOT_APPROVAL,
+
+    EMP_BOARD_ONE_DAY,
+    EMP_BOARD_LONG_TERM,
+
+
+    /*
+        ADMIN
+     */
+
+    ADM_HOME_NOT_APPROVAL,
+    ADM_HOME_APPROVAL_COMPLETED,
+    ADM_HOME_WAITING_FOR_FINISH,
+
+    ADM_BOARD_ONE_DAY,
+    ADM_BOARD_LONG_TERM
+
+}
