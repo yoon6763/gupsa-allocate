21146ecc67c681dea468cd4ba0138cab87644540
seo0 <qwee0328@naver.com>
2023-02-02 18:18:40 +0900

    스피너 수정

--- app/src/main/java/com/gupsa/allocate/SignupAdmActivity.java
+++ app/src/main/java/com/gupsa/allocate/SignupAdmActivity.java
@@ -5,14 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.gupsa.allocate.databinding.SignupAdmBinding;
 import com.gupsa.allocate.databinding.SignupEmpBinding;
+import com.gupsa.allocate.spiner.CustomSpinnerAdapter;
 import com.gupsa.allocate.utils.Constants;
 import com.gupsa.allocate.utils.UserType;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SignupAdmActivity extends AppCompatActivity {
 
+    private final String TAG = this.getClass().getSimpleName();
+
+    private List<String> list01 = new ArrayList<>();
+    private List<String> list02 = new ArrayList<>();
+    private List<String> list03 = new ArrayList<>();
+    private Spinner spinner01, spinner02, spinner03;
+    private CustomSpinnerAdapter adapter;
+    private String selectedItem;
     SignupAdmBinding binding;
 
     @Override
@@ -26,6 +43,96 @@ public class SignupAdmActivity extends AppCompatActivity {
             Intent intent = new Intent(SignupAdmActivity.this, SignupActivity.class);
             intent.putExtra(Constants.INTENT_EXTRA_USER_TYPE, UserType.EMPLOYEE);
             startActivity(intent);
+        });
+
+        spinner01 = findViewById(R.id.spinner01);
+
+        // 스피너 안에 넣을 데이터 임의 생성
+        for (int i = 1; i < 5; i++) {
+            list01.add("아이템" + i);
+        }
+
+        // 스피너에 붙일 어댑터 초기화
+        adapter = new CustomSpinnerAdapter(this, list01);
+        spinner01.setAdapter(adapter);
+
+        // 스피너 클릭 리스너
+        spinner01.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // 어댑터에서 정의한 메서드를 통해 스피너에서 선택한 아이템의 이름을 받아온다
+                selectedItem = adapter.getItem();
+                Toast.makeText(SignupAdmActivity.this, "선택한 아이템 : " + selectedItem, Toast.LENGTH_SHORT).show();
+                // 어댑터에서 정의하는 게 귀찮다면 아래처럼 구할 수도 있다
+                // getItemAtPosition()의 리턴형은 Object이므로 String 캐스팅이 필요하다
+                String otherItem = (String) spinner01.getItemAtPosition(position);
+                Log.e(TAG, "getItemAtPosition() - 선택한 아이템 : " + otherItem);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //
+            }
+        });
+
+        spinner02 = findViewById(R.id.spinner02);
+
+        // 스피너 안에 넣을 데이터 임의 생성
+        for (int i = 1; i < 5; i++) {
+            list02.add("아이템" + i);
+        }
+
+        // 스피너에 붙일 어댑터 초기화
+        adapter = new CustomSpinnerAdapter(this, list02);
+        spinner02.setAdapter(adapter);
+
+        // 스피너 클릭 리스너
+        spinner02.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // 어댑터에서 정의한 메서드를 통해 스피너에서 선택한 아이템의 이름을 받아온다
+                selectedItem = adapter.getItem();
+                Toast.makeText(SignupAdmActivity.this, "선택한 아이템 : " + selectedItem, Toast.LENGTH_SHORT).show();
+                // 어댑터에서 정의하는 게 귀찮다면 아래처럼 구할 수도 있다
+                // getItemAtPosition()의 리턴형은 Object이므로 String 캐스팅이 필요하다
+                String otherItem = (String) spinner02.getItemAtPosition(position);
+                Log.e(TAG, "getItemAtPosition() - 선택한 아이템 : " + otherItem);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //
+            }
+        });
+
+        spinner03 = findViewById(R.id.spinner03);
+
+        // 스피너 안에 넣을 데이터 임의 생성
+        for (int i = 1; i < 5; i++) {
+            list03.add("아이템" + i);
+        }
+
+        // 스피너에 붙일 어댑터 초기화
+        adapter = new CustomSpinnerAdapter(this, list03);
+        spinner03.setAdapter(adapter);
+
+        // 스피너 클릭 리스너
+        spinner03.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // 어댑터에서 정의한 메서드를 통해 스피너에서 선택한 아이템의 이름을 받아온다
+                selectedItem = adapter.getItem();
+                Toast.makeText(SignupAdmActivity.this, "선택한 아이템 : " + selectedItem, Toast.LENGTH_SHORT).show();
+                // 어댑터에서 정의하는 게 귀찮다면 아래처럼 구할 수도 있다
+                // getItemAtPosition()의 리턴형은 Object이므로 String 캐스팅이 필요하다
+                String otherItem = (String) spinner03.getItemAtPosition(position);
+                Log.e(TAG, "getItemAtPosition() - 선택한 아이템 : " + otherItem);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //
+            }
         });
 
     }
---
+++ app/src/main/java/com/gupsa/allocate/spiner/SpinerSecond.java
@@ -0,0 +1,16 @@
+package com.gupsa.allocate.spiner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gupsa.allocate.R;
+
+public class SpinerSecond extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spiner_second);
+    }
+}
